6d55b6a79510d4e1f27bb148a91f164af3f54ce6
Elijah Miller <elijah.miller@gmail.com>
2009-04-11 17:19:10 -0400

    Removing method that creeped back in after a merge.

--- lib/port/game.rb
+++ lib/port/game.rb
@@ -120,21 +120,6 @@ class Game
     end
   end
 
-
-  def clear_landing_strip
-    num = 0
-
-    objects.each do |object|
-      if object.respond_to?(:landed?) && object.landed?
-        object.destroy
-        num += 1
-      end
-    end
-
-    num
-  end
-
-
   def draw
     objects.each do |e|
       e.draw
