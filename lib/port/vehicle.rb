140f637358f5ce85654f8f464541ab1745b3561f
Zelenak <czelenak@czelenak.local>
2009-04-11 14:58:42 -0400

    WOOOO i am mathtarded, also removed accleration cruft

--- lib/port/vehicle.rb
+++ lib/port/vehicle.rb
@@ -5,16 +5,11 @@ class Vehicle < Sprite
 
   class << self
 
-    def max_acceleration()
-      Vector[5, 5]
-    end
-
     def terminal_velocity()
-      Vector[2, 2]
+      Vector[20, 20]
     end
     
   end
-
 
   def heading
     @heading || Vector[0, 0]
@@ -23,8 +18,13 @@ class Vehicle < Sprite
   def heading=(v)
     @heading = v
     position = Vector[self.x, self.y]
-    @time_until = position.distance_to(v) / self.velocity.magnitude()
     self.angle = position.angle_between_gosu(v)
+    result = v - position
+    self.velocity = result.unit * self.velocity.magnitude
+  end
+
+  def angle=(a)
+    @angle =a
   end
 
   def is_heading_to_point?
@@ -32,9 +32,7 @@ class Vehicle < Sprite
   end
 
   def acceleration=(v)
-    if v.magnitude > self.class.max_acceleration.magnitude
-      @acceleration = self.class.max_acceleration.dup
-    else
+    if v.magnitude < self.class.max_acceleration.magnitude
       @acceleration = v
     end
   end
@@ -44,61 +42,32 @@ class Vehicle < Sprite
   end
 
   def velocity=(v)
-    if v.magnitude > self.class.terminal_velocity.magnitude
-      @velocity = self.class.terminal_velocity.dup
-    else
-      @velocity = v
-    end
+    @velocity = v
   end
 
   def velocity
     @velocity || Vector[0,0]
   end
-  
 
-  def update(ts)
-    update_physics(ts)
-
-    if is_heading_to_point?
-      self.x = interpolate(self.x, self.heading.x, @time_until, ts)
-      self.y = interpolate(self.y, self.heading.y, @time_until, ts)
-    else
-      self.x += self.velocity.x
-      self.y += self.velocity.y
-    end
-  end
-  
   def landed?
     window.field.in_landing_zone?(x, y)
   end
 
+  def update(ts, ms)
+    update_physics(ts, ms)
+  end
+
   private
 
-  def update_physics(ts)
-    @last_update ||= 0
-    @last_update += ts
-    if @last_update > 1000 # update physics every 1s
-      self.velocity += self.acceleration        
-      @last_update = 0
-    end
+  def update_physics(ts, ms)
+    v = self.velocity * ms
+    self.x += v.x
+    self.y += v.y
   end
 
-  # A is the initial position, at time 0
-  # B is the target position, to reach at time P
-  # t is the current time, in the range 0..P
-  def interpolate(a, b, p, t)
-    clamp(t, 0, p) * (b-a) / p + a
-  end
-
-  def clamp(val, lower, higher)
-    if val < lower
-      lower
-    elsif val > higher
-      higher
-    else
-      val
-    end
-  end
+  # I removed the interpolation code
+  # because
+  # I HATE BABIES
 
 end
 
