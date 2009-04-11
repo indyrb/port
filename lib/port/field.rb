45e22baac72c2f2e92622514017351c8c20d2808
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-04-11 12:00:42 -0400

    Adding a "landing_zone" on field and a in_landing_zone?(x,y) method.

--- lib/port/field.rb
+++ lib/port/field.rb
@@ -10,4 +10,14 @@ class Field
   def draw
     background.draw(0, 0, 0)
   end
+  
+  def landing_zones
+    [[[215, 245], [240, 496]]]
+  end
+  
+  def in_landing_zone?(x, y)
+    landing_zones.any? do |landing_zone|
+      Range.new(*landing_zone.first).include?(x) && Range.new(*landing_zone.last).include?(y)
+    end
+  end
 end
