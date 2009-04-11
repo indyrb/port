031da697d21ba2b438f86a49da4589d78a78dba5
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-04-11 11:11:33 -0400

    Cleaning up assets class. preloading the assets on initialize.

--- lib/port/assets.rb
+++ lib/port/assets.rb
@@ -2,29 +2,28 @@ class Assets
 
   def initialize(window)
     @window = window
+    preload
   end
   
   def self.asset_dirs
-    @asset_directories
+    @asset_directories ||= add_asset_dir(File.join(APP_ROOT, "images"))
   end
 
   def self.add_asset_dir(dir)
     @asset_directories ||= []
     @asset_directories << dir
   end
-  add_asset_dir(File.join(APP_ROOT, "images")  
+
 
   def preload
-    Assets.asset_dirs.each do |d|
-      Dir.entries(d).each do |f|
-        rpath = File.join(d, f)
-        unless File.directory?(rpath)
-          @assets ||= { }
-          @assets[f] = Gosu::Image.new(@window, rpath)
-          Centipede.logger.info("Loaded #{rpath}")
-        end
-      end
+    @assets ||= {}
+    Dir.glob(File.join(self.class.asset_dirs, '*')).each do |file|
+      @assets[File.basename(file).gsub(File.extname(file), '')] = Gosu::Image.new(@window, file)
     end
+  end
+
+  def list
+    @assets
   end
 
   def by_name(name)
