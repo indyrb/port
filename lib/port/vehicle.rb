a42d1f40927fc05b5e3ae24d142f0cfae8d05cd3
Zelenak <czelenak@czelenak.local>
2009-04-11 10:56:12 -0400

    stub out some phys stuff

--- lib/port/vehicle.rb
+++ lib/port/vehicle.rb
@@ -1,5 +1,22 @@
 class Vehicle < Sprite
+  
   score 1
   z_order 1
 
+  class << self
+
+    def max_acceleration()
+    end
+    
+  end
+
+  attr_accessor :velocity, :acceleration, :heading
+  
+
+  def update
+
+  end
+
 end
+
+
